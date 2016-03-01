72d22e5f08c49046a07e2a58f773f0a42649427c
Ubuntu <ubuntu@ip-172-31-15-78.ec2.internal>
2016-03-01 10:42:22 +0000

    added evaluation

--- examples/encoder-decoder-coupling.lua
+++ examples/encoder-decoder-coupling.lua
@@ -11,9 +11,9 @@ version = 1.2 -- refactored numerical gradient test into unit tests. Added train
 local opt = {}
 opt.learningRate = 0.1
 opt.hiddenSize = 6
-opt.vocabSize = 5
-opt.seqLen = 3 -- length of the encoded sequence
-opt.niter = 1000
+opt.vocabSize = 34
+opt.seqLen = 9 -- length of the encoded sequence
+opt.niter = 150
 
 --[[ Forward coupling: Copy encoder cell and output to decoder LSTM ]]--
 local function forwardConnect(encLSTM, decLSTM)
@@ -47,22 +47,46 @@ dec:add(nn.Sequencer(nn.LogSoftMax()))
 local criterion = nn.SequencerCriterion(nn.ClassNLLCriterion())
 
 -- Some example data (batchsize = 2)
-local encInSeq = torch.Tensor({{1,2,3},{3,2,1}}) 
-local decInSeq = torch.Tensor({{1,2,3,4},{4,3,2,1}})
-local decOutSeq = torch.Tensor({{2,3,4,1},{1,2,4,3}})
+local encInSeq = torch.Tensor({{1,2,3,5,4,1,2,4,10},{3,2,1,4,5,13,24,23,31}}) 
+local decInSeq = torch.Tensor({{1,1,1,1,1},{1,1,1,1,1}})
+local decOutSeq = torch.Tensor({{2,3,4,1,5},{1,2,4,3,5}})
 decOutSeq = nn.SplitTable(1, 1):forward(decOutSeq)
 
-for i=1,1000 do
+for iteration=1,opt.niter do
    enc:zeroGradParameters()
    dec:zeroGradParameters()
 
    -- Forward pass
    local encOut = enc:forward(encInSeq)
-   forwardConnect(encLSTM, decLSTM)
+   forwardConnect(encLSTM, decLSTM) -- Copy encoder output into decoder input
    local decOut = dec:forward(decInSeq)
    local err = criterion:forward(decOut, decOutSeq)
    
-   print(string.format("Iteration %d ; NLL err = %f ", i, err))
+   print(string.format("Iteration %d ; NLL err = %f ", iteration, err))
+   local batchSize = 2
+   if (iteration%1==0) then
+      --print(inputs)
+      local batch_i = 1
+         for batch_i=1, batchSize do
+         local outseq = ''
+         for i=1,#decOut do
+            val, index = torch.max(decOut[i][batch_i], 1)
+            outseq = outseq .. ', ' .. index[1]
+         end
+         local inseq = ''
+         --print(encInSeq:size(2))
+         for i=1, encInSeq:size(2) do
+            inseq = inseq .. ',' ..  encInSeq[batch_i][i]
+         end
+         local targetseq = ''
+         for i=1,#decOutSeq do
+            targetseq = targetseq .. ',' .. decOutSeq[i][batch_i]
+         end
+         print(batch_i .. ': ' .. inseq .. ' -> ' .. outseq .. '('.. targetseq ..')')
+      --print(targets)
+      end
+   end
+
 
    -- Backward pass
    local gradOutput = criterion:backward(decOut, decOutSeq)
